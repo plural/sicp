2215074ded5dd07b54eac651219a39f4d949ec3f
Jason Gessner <jason@multiply.org>
2011-04-24 14:08:39 -0500

    Added a placeholder for exercise 1.13.

---
+++ exercise-1.13.scm
@@ -0,0 +1,8 @@
+#lang planet neil/sicp
+
+; TODO(jgessner): talk to paul and matthias about this proof
+
+; Prove that Fib(n) is the closest integer to (theta?)n/sqrt(5), where
+; theta? = (1 + sqrt(5))/2.
+; Hint: Let ??? = (1 - sqrt(5))/2. Use induction and the definition of the Fibonacci
+; numbers (see section 1.2.2) to prove that Fib(n) = (theta?^n - ???^n)/5.
