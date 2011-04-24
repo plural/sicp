0bc3a2c830f695d59d73843813b048a4f77839de
Jason Gessner <jason@multiply.org>
2011-04-24 14:03:02 -0500

    Clean up the recursive pascals triangle implementation and git rid of the flawed iterative version.

--- exercise-1.12.scm
+++ exercise-1.12.scm
@@ -1,3 +1,4 @@
+#lang planet neil/sicp
 ; Exercise 1.12.  The following pattern of numbers is called Pascal's triangle.
 ; 1                    (1,1)
 ; 1 1                  (2,1) (2,2)
@@ -14,28 +15,10 @@
 (define (pascals-triangle row col)
   (cond ((or (= col 1) (= row col)) 1)
         ((or (= col 2) (= (dec row) col)) (dec row))
-        (else (+ 
+        (else (+
                (pascals-triangle (dec row) (dec col))
                (pascals-triangle (dec row) col)))))
 
-(define (pascals-iterative row col)
-  (define (p-i i j a b c d)
-    (display i) (display " ") (display j) (display " ")
-    (display a) (display " ") (display b) (display " ")
-    (display c) (display " ") (display d) (display "\n")
-    (cond 
-      ; i, j == row, col, so we are done!
-      ((and (= i row) (= col j)) (+ b c))
-      (else (p-i (if (> row i) (inc i) i)
-                 (if (> col j) (inc j) j)
-                 (if (= col j) a (+ a b)) ; <<<<<<<<<<<<<
-                 (if (= col j) (+ a b) (+ b c))
-                 (if (= col j) (+ b c) (+ c d))
-                 (if (and (> col j) (= (- i j) 2)) 1 (+ c d))))))
-  
-  (cond ((or (= col 1) (= row col)) 1)
-        ((or (= col 2) (= col (dec row))) (dec row))
-        ; start at 5,3 because its the first starting point without
-        ; a short-circuit
-        (else (p-i 5 3 1 3 3 1))))
-         
+; TODO(jgessner): write the iterative version of this function as well.
+; The iterative version of this function requires lists, which i don't yet know
+; how to do in scheme.  :)
